9c7cbad8c5039a793cd33ef24470e5ce6d6c96be
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-10-16 08:35:29 -0600

    Comentarios en el codigo

--- suma.cpp
+++ suma.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    // declarar variables
 int a, b, r;
+//leemos variables
 cin>>a>>b;
+//formula para el resultado
 r=a+b;
+//imprimimos formula
 cout<<r;
 
 
