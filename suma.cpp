c1ec9f7c150f18f5d6d45a098a52fb0ffa600812
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-10-16 08:36:27 -0600

    Le agrege una variable, ahora se suman 3

--- suma.cpp
+++ suma.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    // declarar variables
-int a, b, r;
-//leemos variables
-cin>>a>>b;
-//formula para el resultado
-r=a+b;
-//imprimimos formula
+
+int a, b, c, r;
+
+cin>>a>>b>>c;
+
+r=a+b+c;
+
 cout<<r;
 
 
